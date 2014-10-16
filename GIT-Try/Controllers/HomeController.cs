ad0c2afc2eacd7d546e379d1f025f6bfe7ee5450
Jeanaes <seann.x.liu@gmail.com>
2014-10-16 23:50:51 +1300

    UPDATE 1ST

--- GIT-Try/Controllers/HomeController.cs
+++ GIT-Try/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace GIT_Try.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "1ST CHANGE.";
 
             return View();
         }
